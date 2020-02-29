bee56fb968550d7a22155d683f662ab256f097ce
Agustin Palmieri <agujex@Agustins-MBP.utopia.net>
2020-02-29 09:45:04 -0300

    create project structure

---
+++ Aplication/src/test/java/com/aguja/app/AplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aguja.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
